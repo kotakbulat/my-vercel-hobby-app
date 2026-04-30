fbe5ef42e20ba049dbfa93b11d3aabb155bf0ba8
kotakbulat <hokilimpah@gmail.com>
2026-04-30 12:28:36 +0700

    feat: update Widget registry in 03-modular-dashboard

--- src/projects/03-modular-dashboard/core/WidgetRegistry.tsx
+++ src/projects/03-modular-dashboard/core/WidgetRegistry.tsx
@@ -2,10 +2,14 @@ import { NotepadWidget } from '../widgets/Notepad/NotepadWidget';
 import { ClockWidget } from '../widgets/Clock/ClockWidget';
 import { StopwatchWidget } from '../widgets/Stopwatch/StopwatchWidget';
 import { BarChartWidget } from '../widgets/BarChart/BarChartWidget';
+import { LineChartWidget } from '../widgets/D3/LineChartWidget';
+import { PieChartWidget } from '../widgets/D3/PieChartWidget';
 
 export const WidgetRegistry: Record<string, { title: string; component: React.FC<any> }> = {
   notepad: { title: 'Notepad', component: NotepadWidget },
   clock: { title: 'Clock', component: ClockWidget },
   stopwatch: { title: 'Stopwatch', component: StopwatchWidget },
-  barchart: { title: 'Analytics', component: BarChartWidget },
+  barchart: { title: 'Bar Chart', component: BarChartWidget },
+  linechart: { title: 'Line Chart', component: LineChartWidget },
+  piechart: { title: 'Pie Chart', component: PieChartWidget },
 };
