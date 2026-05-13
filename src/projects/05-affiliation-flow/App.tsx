85e7fbe158e752892e1b3ae120f323a4b30ef09b
kotakbulat <hokilimpah@gmail.com>
2026-05-13 19:11:45 +0700

    feat: update App.tsx in project 5

---
+++ src/projects/05-affiliation-flow/App.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './styles/global.css';
+import './styles/animations.css';
+import './styles/components.css';
+
+import { Navbar } from './components/Navbar';
+import { HeroSection } from './components/HeroSection';
+import { RegisterSection } from './components/RegisterSection';
+import { GenerateCodeSection } from './components/GenerateCodeSection';
+import { ShareSection } from './components/ShareSection';
+import { OrderFlowSection } from './components/OrderFlowSection';
+import { TimelineSection } from './components/TimelineSection';
+import { RewardDashboard } from './components/RewardDashboard';
+import { BusinessFlowDiagram } from './components/BusinessFlowDiagram';
+import { CTASection } from './components/CTASection';
+import { Footer } from './components/Footer';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <Navbar />
+      <HeroSection />
+      <RegisterSection />
+      <GenerateCodeSection />
+      <ShareSection />
+      <OrderFlowSection />
+      <TimelineSection />
+      <RewardDashboard />
+      <BusinessFlowDiagram />
+      <CTASection />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
